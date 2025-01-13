4053ba95f534a0d8feebef8d5d185b5c210ae709
anas <muhammedanasmp2001@gmail.com>
2025-01-13 18:09:10 +0530

    PG database connected

--- iggrambackend/settings.py
+++ iggrambackend/settings.py
@@ -37,6 +37,7 @@ CLOUDINARY = {
 }
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG=False
 if DEBUG:
     ALLOWED_HOSTS = ['*']
     CORS_ALLOWED_ORIGINS = [
@@ -125,13 +126,24 @@ WSGI_APPLICATION = 'iggrambackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG == "True":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get("POSTGRES_DB_NAME"),
+            'USER': os.environ.get("POSTGRES_USER"),
+            'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+            'HOST': os.environ.get("POSTGRES_HOST"),
+            'PORT': os.environ.get("POSTGRES_PORT"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
